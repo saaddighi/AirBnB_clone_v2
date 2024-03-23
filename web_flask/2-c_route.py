ae823c12871ae975e0dc438f80090bb82ef13ec7
saaddighi <saaddighi20@gmail.com>
2024-03-23 18:08:30 +0000

    ok

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,8 +21,8 @@ def diplay():
 def dplay():
     """Return string when route queried
     """
-    return 'C' + text.replace('_',' ')
+    return 'C ' + text.replace('_',' ')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
