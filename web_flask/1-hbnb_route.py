0e50f88db4b5efb626d06ded93b658711798c7c5
saaddighi <saaddighi20@gmail.com>
2024-03-23 18:00:42 +0000

    ok

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,18 +8,18 @@ from flask import  Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def display():
     """Return string when route queried
     """
     return 'Hello HBNB!'
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes = False)
 def diplay():
     """Return string when route queried
     """
     return 'HBNB'
 
 if __name__ == "__main__":
-    app.url_map.strict_slashes = False
+    strict_slashes = False
     app.run(host='0.0.0.0', port= 5000)
