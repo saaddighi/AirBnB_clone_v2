39aae3f97c6b810801a99d092f72fc82c8b4ecd5
saaddighi <saaddighi20@gmail.com>
2024-03-23 18:12:54 +0000

    ok

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,25 +12,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
     """Return string when route queried
     """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Return string when route queried
     """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """Return reformatted text
     """
     return 'C ' + text.replace('_', ' ')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
