7fd772cbd1046bd0c0757aae1e7ee86ce389e5bc
saaddighi <saaddighi20@gmail.com>
2024-03-23 17:23:31 +0000

    ok

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Start web application with multiple routings
+"""
+from flask import Flask
+
+app = Flask(__name__)
+text = 'is cool'
+
+@app.route('/')
+def  display():
+    """Return string when route queried
+    """
+    return '“Hello HBNB!”'
+
+@app.route('/hbnb')
+def diplay():
+    """Return string when route queried
+    """
+    return '“HBNB”'
+
+@app.route('/c/<text>')
+def dplay():
+    """Return string when route queried
+    """
+    return 'C' + text.replace('_', ' ')
+
+@app.route('/python/<text>')
+def dlay():
+    """Return string when route queried
+    """
+    return 'Python' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
