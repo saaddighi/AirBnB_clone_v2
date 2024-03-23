81325aa21db18930a0b483a089b8093ef721b946
saaddighi <saaddighi20@gmail.com>
2024-03-23 18:10:25 +0000

    ok

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""Start web application with two routings
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 
 from flask import Flask
