7eebaba67c7ea49497af3ece6ffeda46c3b689a1
saaddighi <saaddighi20@gmail.com>
2024-03-23 18:14:59 +0000

    ok

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,30 +4,30 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def  display():
     """Return string when route queried
     """
     return 'Hello HBNB!'
 
-@app.route('/hbnb')
+@app.route("/hbnb")
 def diplay():
     """Return string when route queried
     """
     return 'HBNB'
 
-@app.route('/c/<text>')
+@app.route("/c/<text>")
 def dplay(text):
     """Return string when route queried
     """
     return 'C ' + text.replace('_', ' ')
 
-@app.route('/python/<text>')
-def dlay(text = 'is cool'):
+@app.route("/python/<text>")
+def dlay(text = "is cool"):
     """Return string when route queried
     """
     return 'Python ' + text.replace('_', ' ')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
