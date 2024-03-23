91a16dff214c61280cc08ceb8f39d7ec6f2eec14
saaddighi <saaddighi20@gmail.com>
2024-03-23 18:04:38 +0000

    ok

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,12 +7,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def display():
-    """Return string when route queried
-    """
-    return "“Hello HBNB!”"
+    """Return string when route queried"""
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
