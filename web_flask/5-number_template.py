522b477b016643cc1982aa79ec155a54863c5b2e
saaddighi <saaddighi20@gmail.com>
2024-03-27 18:33:29 +0000

    ok

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Start web application with multiple routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def diplay():
+    """Return string when route queried
+    """
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def dplay(text):
+    """Return string when route queried
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def dlay(text="is cool"):
+    """Return string when route queried
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def dpl(n):
+    "return string if n is a number"
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def ply():
+    "return html file if n is a number"
+    return render_template('5-number.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
