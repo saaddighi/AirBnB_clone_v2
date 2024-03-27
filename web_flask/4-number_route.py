29cb029055981963d925a12fba85a90ad3d7446f
saaddighi <saaddighi20@gmail.com>
2024-03-27 18:06:56 +0000

    ok

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Start web application with multiple routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def diplay():
+    """Return string when route queried
+    """
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def dplay(text):
+    """Return string when route queried
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def dlay(text="is cool"):
+    """Return string when route queried
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number', strict_slashes=False)
+@app.route('/number/<n>', strict_slashes=False)
+def dpl(n):
+    if type(n) == int():
+        return "n is a number"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+ 
