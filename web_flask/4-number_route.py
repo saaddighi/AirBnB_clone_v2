9c9beb98a47cbfb6c8488934a96dba0f78c54c73
saaddighi <saaddighi20@gmail.com>
2024-03-27 18:14:20 +0000

    ok

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,12 +35,10 @@ def dlay(text="is cool"):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number', strict_slashes=False)
-@app.route('/number/<n>', strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def dpl(n):
     "return string if n is a number"
-    if type(n) == type(1):
-        return str(n) +" is a number"
+    return f"{n} is a number"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
